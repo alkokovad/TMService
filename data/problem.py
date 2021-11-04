480db19886730c3d426fbad613d1a2430a869c95
AlkoKovad <sanya.dobriy.sosed@mail.ru>
2021-11-04 16:31:10 +0300

    db fix

--- data/problem.py
+++ data/problem.py
@@ -3,11 +3,11 @@ import sqlalchemy
 from .db_session import SqlAlchemyBase
 
 
-class User(SqlAlchemyBase):
-    __tablename__ = 'users'
+class Problems(SqlAlchemyBase):
+    __tablename__ = 'problems'
 
-    id = sqlalchemy.Column(sqlalchemy.Integer,
-                           primary_key=True, autoincrement=True)
+    id = sqlalchemy.Column(sqlalchemy.INTEGER,
+                           primary_key=True, autoincrement=False)
 
     description = sqlalchemy.Column(sqlalchemy.Text,
                                     primary_key=True, autoincrement=False)
