2000852be2a97bc4e7bba62b4649cc11449bca9e
User <vvv@yyyy.ru>
2021-11-04 17:59:27 +0300

    minor fixes

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 from flask import Flask, render_template
 
-
 app = Flask(__name__)
 
 
-@app.route('/main')
-def main_page():
-    return render_template('base.html')
+@app.route('/')
+def show_map():
+    return render_template('map.html')
 
 
 if __name__ == '__main__':
