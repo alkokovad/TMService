bca04f3f56a00bab0005c74dfd7eadc529202530
so0n <godov@gmail.com>
2021-11-04 23:58:00 +0300

    added one-dot map

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ def show_map():
     return render_template('map.html')
 
 
+@app.route('/onedot/')
+def show_onedot_map():
+    return render_template('mapOneDot.html')
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
 
