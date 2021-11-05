f63aa30021c87560609b5393b94626c1a72a3982
AlkoKovad <sanya.dobriy.sosed@mail.ru>
2021-11-05 04:51:00 +0300

    register

---
+++ data/User.py
@@ -0,0 +1,25 @@
+import datetime
+
+import sqlalchemy
+from sqlalchemy import orm
+
+from .db_session import SqlAlchemyBase
+
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+class User(SqlAlchemyBase):
+    __tablename__ = 'users'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    email = sqlalchemy.Column(sqlalchemy.String,
+                              index=True, unique=True, nullable=True)
+    hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+
+    def set_password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.hashed_password, password)
--- main.py
+++ main.py
@@ -1,9 +1,15 @@
+from data.User import User
 from flask import Flask, render_template
+from forms.registerform import RegisterForm
+from werkzeug.utils import redirect
+
+from data import db_session
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'nazvanie_comandi'
 
 
-@app.route('/')
+@app.route('/map')
 def show_map():
     return render_template('map.html')
 
@@ -13,12 +19,35 @@ def show_onedot_map():
     return render_template('mapOneDot.html')
 
 
-@app.route('/main_page')
+@app.route('/main')
 def main_page():
     return render_template('base.html')
 
 
+@app.route('/', methods=['GET', 'POST'])
+def registration():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть или ваш пароль не подходит")
+        user = User(
+            name=form.name.data,
+            email=form.email.data,
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/main')
+    return render_template('register.html', form=form)
+
+
 if __name__ == '__main__':
     app.run(port=8079, host='127.0.0.1')
-
-
+    db_session.global_init("db/users.db")
