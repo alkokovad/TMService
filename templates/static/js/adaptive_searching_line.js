7d7f8e0afc6d060d84da9596a5245d3b06235b38
AlkoKovad <aleksandr.kopeykin@gmail.com>
2022-03-08 15:42:43 +0300

    base searching and menu canvas

--- templates/static/js/adaptive_searching_line.js
+++ templates/static/js/adaptive_searching_line.js
@@ -1,7 +1,33 @@
-let line = document.querySelector('.line_here');
-if (screen.width > screen.height){
-    line.classList.add('.search_line_wide')
+let line = document.querySelector(".line_here");
+let menu = document.querySelector(".menu_here");
+
+function set_design_for_line(){
+  if (window.screen.width > screen.height){
+    line.classList.remove("search_line_thin");
+    line.classList.add('search_line_wide');
+    console.log("нет, я здесь");
+  }
+  else{
+    line.classList.remove("search_line_wide");
+    line.classList.add('search_line_thin');
+    console.log("я здесь");
+  }
 }
-if (screen.height > screen.width){
-    line.classList.add('.search_line_thin')
+
+function set_design_for_menu(){
+  if (window.screen.width > screen.height){
+    menu.classList.remove("menu_circle_thin");
+    menu.classList.add('menu_circle_wide');
+    console.log("нет, я здесь");
+  }
+  else{
+    menu.classList.remove("menu_circle_wide");
+    menu.classList.add('menu_circle_thin');
+    console.log("я здесь");
+  }
 }
+
+document.addEventListener("DOMContentLoaded", function(event) {window.onresize = function() {
+set_design_for_menu();
+set_design_for_line()
+};});
